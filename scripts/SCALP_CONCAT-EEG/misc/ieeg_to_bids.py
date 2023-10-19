0dd15cdc16819241a30b60a79e7fd5d7718e4c7c
UPennBJPrager <bjprager@seas.upenn.edu>
2023-10-19 08:53:24 -0400

    #86ay8bt5m[Complete] ieeg loader updates

--- scripts/SCALP_CONCAT-EEG/misc/ieeg_to_bids.py
+++ scripts/SCALP_CONCAT-EEG/misc/ieeg_to_bids.py
@@ -2,11 +2,13 @@ import re
 import sys
 import mne
 import glob
+import pickle
 import mne_bids
 import argparse
 import numpy as np
 import pandas as PD
 from os import path
+from tqdm import tqdm
 from time import sleep
 from ieeg.auth import Session
 from mne_bids import BIDSPath, write_raw_bids
@@ -20,6 +22,11 @@ from requests.exceptions import ReadTimeout as RTIMEOUT
 import signal
 class TimeoutException(Exception):
     pass
+
+# For testing, mute mne future warning
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=RuntimeWarning)
 
 class Timeout:
     def __init__(self, seconds=1, error_message='Function call timed out'):
@@ -42,6 +49,7 @@ class BIDS_handler:
         self.raws      = []
         self.data_info = {'iEEG_id':self.current_file}
         self.get_subject_number()
+        self.get_session_number()
 
     def reset_variables(self):
             # Delete all variables in the object's namespace
@@ -50,11 +58,31 @@ class BIDS_handler:
 
     def get_subject_number(self):
 
-        files            = glob.glob(self.args.bidsroot+'sub-*')
-        if len(files) > 0:
-            self.subject_num  = max([int(ifile.split('sub-')[-1]) for ifile in files])+1
+        # Load the mapping if available, otherwise dummy dataframe
+        if not path.exists(self.subject_path):
+            subject_uid_df = PD.DataFrame(np.empty((1,3)),columns=['iEEG file','uid','subject_number'])
         else:
-            self.subject_num = 1
+            subject_uid_df = PD.read_csv(self.subject_path)
+
+        # Check if we already have this subject
+        uids = subject_uid_df['uid'].values
+        if self.uid not in uids:
+            files = glob.glob(self.args.bidsroot+'sub-*')
+            if len(files) > 0:
+                self.subject_num  = max([int(ifile.split('sub-')[-1]) for ifile in files])+1
+            else:
+                self.subject_num = 1
+        else:
+            self.subject_num = int(subject_uid_df['subject_number'].values[np.where(uids==self.uid)[0][0]])
+
+    def get_session_number(self):
+
+        folders = glob.glob("%ssub-%04d" %(self.args.bidsroot,self.subject_num))
+        regex   = re.compile(r"(\D+)(\d+)")
+        if len(folders) > 0:
+            self.session_number = max([int(regex.match(ival).group(2)) for ival in folders])+1
+        else:
+            self.session_number = 1
 
     def get_channel_type(self, threshold=15):
 
@@ -95,10 +123,10 @@ class BIDS_handler:
         self.channel_types = PD.DataFrame(self.channel_types.reshape((-1,1)),index=self.channels,columns=["type"])
 
     def make_info(self):
-        self.data_info = mne.create_info(ch_names=list(self.channels), sfreq=self.fs)
+        self.data_info = mne.create_info(ch_names=list(self.channels), sfreq=self.fs, verbose=False)
 
     def add_raw(self):
-        self.raws.append(mne.io.RawArray(self.data.T, self.data_info))
+        self.raws.append(mne.io.RawArray(self.data.T, self.data_info, verbose=False))
 
     def event_mapper(self):
 
@@ -119,14 +147,25 @@ class BIDS_handler:
                 desc   = self.annotations[idx][itime]
                 index  = (1e-6*itime)*self.fs
                 events = np.array([[int(index),0,self.event_mapping[desc]]])
+
                 try:
-                    bids_path = mne_bids.BIDSPath(root=self.args.bidsroot, datatype='eeg', session=self.args.session, subject='%04d' %(self.subject_num), run=idx+1, task='task')
-                    write_raw_bids(bids_path=bids_path, raw=raw, events_data=events,event_id=self.event_mapping, allow_preload=True, format='EDF',verbose=False)
-                except FileExistsError:
-                    pass
+                    # Save the edf in bids format
+                    session_str = "%s%02d" %(self.args.session,self.session_number)
+                    bids_path   = mne_bids.BIDSPath(root=self.args.bidsroot, datatype='eeg', session=session_str, subject='%04d' %(self.subject_num), run=idx+1, task='task')
+                    write_raw_bids(bids_path=bids_path, raw=raw, events_data=events,event_id=self.event_mapping, allow_preload=True, format='EDF',verbose=False,overwrite=True)
+
+                    # Save the targets with the edf path paired up to filetype
+                    target_path = str(bids_path.copy()).rstrip('.edf')+'_targets.pickle'
+                    target_dict = {'target':self.target,'annotation':desc}
+                    pickle.dump(target_dict,open(target_path,"wb"))
+
+                except (FileExistsError,RuntimeError) as e:
+                    print(e)
+                    pickle.dump(raw,open("raw.pickle","wb"))
+                    exit()
 
         # Save the subject file info
-        iDF = PD.DataFrame([[self.current_file,self.subject_num]],columns=['iEEG file','subject_number'])
+        iDF = PD.DataFrame([[self.current_file,self.uid,self.subject_num]],columns=['iEEG file','uid','subject_number'])
 
         if not path.exists(self.subject_path):
             subject_DF = iDF.copy()
@@ -135,7 +174,6 @@ class BIDS_handler:
             subject_DF = PD.concat((subject_DF,iDF))
         subject_DF['subject_number'] = subject_DF['subject_number'].astype(str).str.zfill(4)
         subject_DF.to_csv(self.subject_path,index=False)
-
 
 class iEEG_handler(BIDS_handler):
 
@@ -192,9 +230,11 @@ class iEEG_handler(BIDS_handler):
                         self.annotations[idx][event_time_shift] = desc
                         self.annotation_flats.append(desc)
         
-    def download_by_annotation(self, file):
+    def download_by_annotation(self, uid, file, target):
 
         # Store the ieeg filename
+        self.uid          = uid
+        self.target       = target
         self.current_file = file
         self.success_flag = False
 
@@ -224,14 +264,53 @@ class iEEG_handler(BIDS_handler):
         self.reset_variables()
 
     def session_method(self,start,duration,annotation_flag):
+        """
+        Call ieeg.org for data and return data or annotations.
+
+        Args:
+            start (float): Start time (referenced to data start) in microseconds to request data from
+            duration (float): Duration in microseconds of data to request
+            annotation_flag (bool, optional): Flag whether we just want annotation data or not. Defaults to False.
+
+        Raises:
+            IndexError: If there are multiple sampling frequencies, bids does not readily support this. Alerts user and stops.
+        """
 
         with Session(self.args.username,self.args.password) as session:
-            dataset       = session.open_dataset(self.current_file)
+            
+            # Open dataset session
+            dataset = session.open_dataset(self.current_file)
+            
+            # Logic gate for annotation call (faster, no time data needed) or get actual data
             if not annotation_flag:
+
+                # Get the channel names and integer representations for data call
                 self.channels = dataset.ch_labels
                 channel_cntr  = list(range(len(self.channels)))
-                self.data     = dataset.get_data(start,duration,channel_cntr)
-                self.fs       = [dataset.get_time_series_details(ichannel).sample_rate for ichannel in self.channels]
+
+                # If duration is greater than 10 min, break up the call. Make array of start,duration with max 10 min each chunk
+                time_cutoff = int(10*60*1e6)
+                end_time    = start+duration
+                ival        = start
+                chunks      = []
+                while ival < end_time:
+                    if ival+time_cutoff >= end_time:
+                        chunks.append([ival,end_time-ival])
+                    else:
+                        chunks.append([ival,time_cutoff])
+                    ival += time_cutoff
+
+                # Call data and concatenate calls if greater than 10 min
+                self.data   = []
+                for ival in chunks:
+                    self.data.append(dataset.get_data(ival[0],ival[1],channel_cntr))
+                if len(self.data) > 1:
+                    self.data = np.concatenate(self.data)
+                else:
+                    self.data = self.data[0]
+                
+                # Get the samping frequencies
+                self.fs = [dataset.get_time_series_details(ichannel).sample_rate for ichannel in self.channels]
 
                 # Data quality checks before saving
                 if np.unique(self.fs).size == 1:
@@ -246,6 +325,14 @@ class iEEG_handler(BIDS_handler):
             session.close()
 
     def session_method_handler(self,start,duration,annotation_flag=False):
+        """
+        Wrapper to call ieeg. Due to ieeg errors, we want to make sure we can try to call it a few times before giving up.
+
+        Args:
+            start (float): Start time (referenced to data start) in microseconds to request data from
+            duration (float): Duration in microseconds of data to request
+            annotation_flag (bool, optional): Flag whether we just want annotation data or not. Defaults to False.
+        """
 
         n_attempts = 0
         while True:
@@ -256,7 +343,6 @@ class iEEG_handler(BIDS_handler):
                     break
                 except (IIA.IeegConnectionError,IIA.IeegServiceError,TimeoutException,RTIMEOUT,TypeError) as e:
                     if n_attempts<self.n_retry:
-                        print("Possible iEEG error. Trying again momentarily.")
                         sleep(5)
                         n_attempts += 1
                     else:
@@ -281,16 +367,17 @@ if __name__ == '__main__':
 
     bids_group = parser.add_argument_group('BIDS options')
     bids_group.add_argument("--bidsroot", type=str, required=True, help="Bids Root Directory.")
-    bids_group.add_argument("--session", type=str, required=True, help="Session Keyword for BIDS.")
+    bids_group.add_argument("--session", type=str, required=True, help="Base string session keyword for BIDS. (i.e. 'preimplant')")
 
     other_group = parser.add_argument_group('Other options')
     other_group.add_argument("--annotation_file", type=str, help="File of iEEG datasets to download by annotation.")
     other_group.add_argument("--subject_file", type=str, default='subject_map.csv', help="File mapping subject id to ieeg file. (Defaults to bidroot+'subject_map.csv)")
+    other_group.add_argument("--uid", type=str, help="Unique patient identifier for single ieeg calls. This is to map patients across different admissions. See sample subject_map.csv file for an example.")
 
     selection_group = parser.add_mutually_exclusive_group()
     selection_group.add_argument("--cli", action='store_true', default=False, help="Use start and duration from this CLI.")
     selection_group.add_argument("--csv", type=str, help="CSV file with filename, start time, and duration.")
-    selection_group.add_argument("--annotations", action='store_true', default=False, help="File with filenames to obtained annotated data from.")
+    selection_group.add_argument("--annotations", action='store_true', default=False, help="CSV file with de-identified unique patient id, ieeg filename, and targets (optional). Format:[uid,ieeg_filename,target]")
     args = parser.parse_args()
 
     # Selection criteria
@@ -311,12 +398,20 @@ if __name__ == '__main__':
             else:
                 processed_files = []
 
+            # Read in the mapping file
+            map_data = PD.read_csv(args.annotation_file)
+
             # Loop over files and process as needed
-            input_files = PD.read_csv(args.annotation_file).values[:,0]
-            for ifile in input_files:
+            input_files = map_data['ieeg_filename'].values
+            for file_idx,ifile in tqdm(enumerate(input_files), desc="Downloading Data:", total=input_files.size):
                 if ifile not in processed_files:
-                    IEEG.download_by_annotation(ifile)
+                    iid    = map_data['uid'].values[file_idx]
+                    target = map_data['target'].values[file_idx]
+                    IEEG.download_by_annotation(iid,ifile,target)
                     IEEG = iEEG_handler(args)
+
+
+                    
 
 
     
