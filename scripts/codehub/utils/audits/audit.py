69745f16ac0037eb2d054943dcc9ae1bbd9e4a56
UPennBJPrager <bjprager@seas.upenn.edu>
2024-02-12 09:12:20 -0500

    Update audit.py

--- scripts/codehub/utils/audits/audit.py
+++ scripts/codehub/utils/audits/audit.py
@@ -14,6 +14,7 @@ class audit:
         self.outdir        = outdir
         self.os            = os
         self.cmd_path      = cmd_path
+        self.delimiter     = '.'
 
         # Some basic cleanup
         if self.rootdir[-1] != '/':
@@ -26,7 +27,8 @@ class audit:
             self.audit_history = audit_history
         else:
             self.audit_history = self.outdir+'audit_history.csv'
-            self.lock_file     = self.outdir+'audit_history.lock'
+        self.lock_file  = self.outdir+'audit_history.lock'
+        self.audit_data = self.outdir+'audit_data.csv'
 
     def argcheck(self):
 
@@ -53,7 +55,7 @@ class audit:
         for content in root_contents:
             if os.path.isdir(content):
                 folders.append(content)
-        folders = np.sort(folders)
+        self.folders = np.sort(folders)
 
         # Read in the audit history
         if os.path.exists(self.audit_history):
@@ -64,7 +66,7 @@ class audit:
         # Check for already completed entries
         self.input_paths  = []
         completed_folders = self.history['directory_path'].values
-        for ifolder in folders:
+        for ifolder in self.folders:
             alt_path = str(ifolder) + '/'
             if ifolder not in completed_folders and alt_path not in completed_folders:
                 self.input_paths.append(str(ifolder))
@@ -78,6 +80,9 @@ class audit:
                 self.input_paths[idx] += '/'
 
     def perform_audit_linux(self):
+        """
+        Perform a data audit on a linux/unix filesystem.
+        """
 
         if not os.path.exists(self.outdir):
             os.system(f"mkdir -p {self.outdir}")
@@ -91,9 +96,8 @@ class audit:
             instr = ifolder
 
             # Modify the input directory name to make an output filename
-            delimiter = '.'
-            self.outname   = f"{ifolder.replace('/',delimiter)[:-1]}.audit"
-            if self.outname[0] == delimiter:
+            self.outname   = f"{ifolder.replace('/',self.delimiter)[:-1]}.audit"
+            if self.outname[0] == self.delimiter:
                 self.outname = self.outname[1:]
             self.outname = f"{self.outdir}{self.outname}"
 
@@ -127,6 +131,46 @@ class audit:
             except:
                 pass
 
+    def clean_audit(self):
+        """
+        Clean up the audit outputs to one csv file.
+        """
+
+        # Make or load the audit data file as needed
+        if not os.path.exists(self.audit_data):
+            fp = open(self.audit_data,'w')
+            fp.write('path,md5,size-(MB),last-modified-date\n')
+        else:
+            fp = open(self.audit_data,'a')
+
+        # Add the root directory to the search path, then loop
+        folder_list = [str(ifolder) for ifolder in self.folders]
+        folder_list.append(str(self.rootdir))
+        for ifolder in folder_list:
+
+            # Get the expected target output file for this folder
+            self.outname   = f"{ifolder.replace('/',self.delimiter)}.audit"
+            if self.outname[0] == self.delimiter:
+                self.outname = self.outname[1:]
+            self.outname = f"{self.outdir}{self.outname}"
+            self.outname = self.outname.replace('..audit','.audit')
+
+            # If file exists, add it to the data file then remove
+            if os.path.exists(self.outname):
+                
+                # Read in and clean the raw audit data
+                fp2     = open(self.outname,'r')
+                rawdata = np.array(fp2.readlines())
+                rawdata = rawdata.reshape((-1,3))
+                for irow in rawdata:
+                    rawstring  = ' '.join(irow)
+                    clean_data = rawstring.replace('\n','').replace(' MB','').replace(' ',',')
+                    fp.write(f"{clean_data}\n")
+                    rawdata = fp2.readline()
+                fp2.close()
+                os.remove(self.outname)
+        fp.close()
+
 if __name__ == '__main__':
     
     # Command line options needed to obtain data.
@@ -145,5 +189,5 @@ if __name__ == '__main__':
     AH.define_inputs()
     if args.os.lower() == 'unix':
         AH.perform_audit_linux()
-
+    AH.clean_audit()
     
