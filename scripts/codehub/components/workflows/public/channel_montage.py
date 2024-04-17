70e87a542bb4bf1ff32a5d87eb20222a161af465
UPennBJPrager <bjprager@seas.upenn.edu>
2024-04-17 08:11:55 -0400

    Update channel_montage.py

--- scripts/codehub/components/workflows/public/channel_montage.py
+++ scripts/codehub/components/workflows/public/channel_montage.py
@@ -183,14 +183,8 @@ class channel_montage:
                     else:
                         raise ValueError("Cannot montage channels with different frequencies.")
                 except IndexError:
-                    self.new_fs.append(np.nan)
+                    self.new_fs.append(fs0)
             
-            # Update in place the frequencies that did not update correctly
-            self.fs          = np.array(self.fs)
-            naninds          = np.isnan(self.new_fs)
-            newval           = np.unique(self.new_fs[~naninds])[0]
-            self.fs[naninds] = newval
-
         # Get the new montage channel labels
         self.montage_channels = [f"{ichannel[0]}-{ichannel[1]}" for ichannel in bipolar_array]
 
