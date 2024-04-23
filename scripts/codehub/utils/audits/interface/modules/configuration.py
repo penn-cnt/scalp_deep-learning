fe30c28db8c18fd298ed6c5c826f60b6ee7246be
UPennBJPrager <bjprager@seas.upenn.edu>
2024-04-23 12:46:58 -0400

    Misc

--- scripts/codehub/utils/audits/interface/modules/configuration.py
+++ scripts/codehub/utils/audits/interface/modules/configuration.py
@@ -7,6 +7,15 @@ class configuration_handler:
     def __init__(self):
         pass
 
+    def update_leifborel(self,sender,app_data):
+        self.show_all_data(dpg.get_value(self.leifborel_input_path_widget_text),self.leifborel_text_id,'leifborel')
+
+    def update_bsc(self,sender,app_data):
+        self.show_all_data(dpg.get_value(self.bsc_input_path_widget_text),self.bsc_text_id,'bsc')
+
+    def update_habitat(self,sender,app_data):
+        self.show_all_data(dpg.get_value(self.habitat_input_path_widget_text),self.habitat_text_id,'habitat')
+
     def showConfiguration(self, main_window_width = 1280):
 
         # Load the default paths
@@ -25,14 +34,17 @@ class configuration_handler:
             dpg.add_text(f"{'Leif/Borel Audit Data':{str_width}}")
             self.leifborel_input_path_widget_text = dpg.add_input_text(default_value=default_leif)
             self.leifborel_input_path_widget      = dpg.add_button(label="Select File", width=int(0.1*main_window_width), callback=lambda sender, app_data:self.init_file_selection(self.leifborel_input_path_widget_text, sender, app_data))
+            self.leifborel_update_path            = dpg.add_button(label="Update", width=int(0.1*main_window_width), callback=self.update_leifborel)
         with dpg.group(horizontal=True):
             dpg.add_text(f"{'BSC Audit Data':{str_width}}")
             self.bsc_input_path_widget_text = dpg.add_input_text(default_value=default_bsc)
             self.bsc_input_path_widget      = dpg.add_button(label="Select File", width=int(0.1*main_window_width), callback=lambda sender, app_data:self.init_file_selection(self.bsc_input_path_widget_text, sender, app_data))
+            self.bsc_update_path            = dpg.add_button(label="Update", width=int(0.1*main_window_width), callback=self.update_bsc)
         with dpg.group(horizontal=True):
             dpg.add_text(f"{'Habitat Audit Data':{str_width}}")
             self.habitat_input_path_widget_text = dpg.add_input_text(default_value=default_habitat)
             self.habitat_input_path_widget      = dpg.add_button(label="Select File", width=int(0.1*main_window_width), callback=lambda sender, app_data:self.init_file_selection(self.habitat_input_path_widget_text, sender, app_data))
+            self.habitat_update_path            = dpg.add_button(label="Update", width=int(0.1*main_window_width), callback=self.update_habitat)
 
         self.show_all_data(dpg.get_value(self.leifborel_input_path_widget_text),self.leifborel_text_id,'leifborel')
         self.show_all_data(dpg.get_value(self.bsc_input_path_widget_text),self.bsc_text_id,'bsc')
