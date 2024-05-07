a32305a3e736242f11447bd7874d29fa2a141aec
UPennBJPrager <bjprager@seas.upenn.edu>
2024-05-07 11:20:40 -0400

    Update iEEG_handler.py

--- scripts/codehub/utils/acquisition/BIDS/modules/iEEG_handler.py
+++ scripts/codehub/utils/acquisition/BIDS/modules/iEEG_handler.py
@@ -311,9 +311,7 @@ class ieeg_handler:
             list_subsets[self.args.ncpu-1] = np.concatenate((arr_ncpu,arr_ncpu1), axis=0)
             list_subsets.pop(-1)
 
-        print(file_indices)
-        print(self.input_files)
-        print(self.input_files.size)
+        print(list_subsets)
         exit()
 
         processes = []
