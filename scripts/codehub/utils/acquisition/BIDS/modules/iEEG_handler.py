d2cd318ac71501647cf786283b9961087aa5962f
UPennBJPrager <bjprager@seas.upenn.edu>
2024-05-07 10:03:39 -0400

    Update iEEG_handler.py

--- scripts/codehub/utils/acquisition/BIDS/modules/iEEG_handler.py
+++ scripts/codehub/utils/acquisition/BIDS/modules/iEEG_handler.py
@@ -332,7 +332,7 @@ class ieeg_handler:
             iid    = self.input_data['uid'].values[file_idx]
             target = self.input_data['target'].values[file_idx]
 
-            print(f"++++{ifile}+++")
+            print(f"++++{file_idx,file_indices}+++")
 
             if self.args.annotations:
                 IEEG.download_by_annotation(iid,ifile,target,self.proposed_sub[file_idx])
