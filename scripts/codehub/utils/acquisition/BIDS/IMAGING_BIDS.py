ee272d2daa3d70de60f82d33760af6dccc6b0340
UPennBJPrager <bjprager@seas.upenn.edu>
2024-10-02 09:02:32 -0400

    Update IMAGING_BIDS.py

--- scripts/codehub/utils/acquisition/BIDS/IMAGING_BIDS.py
+++ scripts/codehub/utils/acquisition/BIDS/IMAGING_BIDS.py
@@ -161,7 +161,7 @@ class prepare_imaging:
         Pathlib(rootpath).mkdir(parents=True, exist_ok=True)
         
         # Copy the different data files over
-        root_file = ifile.split('.')[:-1]
+        root_file = '.'.join(ifile.split('.')[:-1])
         all_files = glob.glob(f"{root_file}*")
         print(root_file)
         exit()
