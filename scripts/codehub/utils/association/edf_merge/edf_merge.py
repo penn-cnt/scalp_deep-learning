63c38deed38b871b54b415419af49ecee4482e79
UPennBJPrager <bjprager@seas.upenn.edu>
2024-10-11 10:20:30 -0400

    Update edf_merge.py

--- scripts/codehub/utils/association/edf_merge/edf_merge.py
+++ scripts/codehub/utils/association/edf_merge/edf_merge.py
@@ -50,7 +50,9 @@ if __name__ == '__main__':
         outraw = read_raw_edf(iblock[0])
         for ifile in tqdm(iblock[1:], total=len(iblock)-1, desc=f"Merging Block {idx:02d}"):
             newraw = read_raw_edf(ifile)
-            print(newraw)
+            print(newraw.get_data())
+            print(newraw.info)
+            print(outraw.info)
             exit()
             outraw = concatenate_raws([outraw,newraw])
         export_raw(f"{args.outdir}merged_{idx:03d}.edf",outraw,fmt='edf')
