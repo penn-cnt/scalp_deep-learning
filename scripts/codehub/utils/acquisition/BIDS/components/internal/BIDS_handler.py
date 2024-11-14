fcdfd02ae402749bfd31aceeb2948f9b80032c06
UPennBJPrager <bjprager@seas.upenn.edu>
2024-11-14 08:58:23 -0500

    Update BIDS_handler.py

--- scripts/codehub/utils/acquisition/BIDS/components/internal/BIDS_handler.py
+++ scripts/codehub/utils/acquisition/BIDS/components/internal/BIDS_handler.py
@@ -133,7 +133,7 @@ class BIDS_handler:
 
         # Save the bids data
         try:
-            write_raw_bids(bids_path=self.bids_path, raw=raw, events_data=self.events,event_id=self.event_mapping, allow_preload=True, format='EDF', overwrite=True, verbose=False)
+            write_raw_bids(bids_path=self.bids_path, raw=raw, events=self.events,event_id=self.event_mapping, allow_preload=True, format='EDF', overwrite=True, verbose=False)
             return True
         except Exception as e:
             if debug:
