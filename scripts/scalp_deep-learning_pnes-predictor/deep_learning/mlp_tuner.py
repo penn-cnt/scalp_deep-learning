a3a96d4d0a9cbe99d707195ad125648c5c741eb0
UPennBJPrager <bjprager@seas.upenn.edu>
2024-12-24 10:28:06 -0500

    Mis

--- scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
+++ scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
@@ -1,4 +1,6 @@
 import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import roc_auc_score
 
 # Torch loaders
 import torch
@@ -121,12 +123,12 @@ class CombinedNetwork(nn.Module):
             output   = self.fc_output(combined)
             output   = self.dropout[idx](output)
             output   = self.sigmoid(output)
-            print(output)
-
+        return output
 
 class ConsensusNetwork(nn.Module):
     def __init__(self, config, input_dict, hidden_dict, dropout_dict, combination_dict, output_size):
         super(ConsensusNetwork, self).__init__()
+
 
         self.config           = config
         self.input_dict       = input_dict
@@ -134,25 +136,104 @@ class ConsensusNetwork(nn.Module):
         self.dropout_dict     = dropout_dict
         self.combination_dict = combination_dict
         self.output_size      = output_size
+        #self.combine_net      = CombinedNetwork(self.input_dict, self.hidden_dict, self.dropout_dict, self.combination_dict, self.output_size, normorder=self.config['normorder'], activation=self.config['activation'])
 
-    def forward(self):
-        CombinedNetwork(self.input_dict, self.hidden_dict, self.dropout_dict, self.combination_dict, self.output_size, normorder=self.config['normorder'], activation=self.config['activation'])
-        exit()
+        self.fc      = nn.Linear(9, output_size)
+        self.relu    = nn.ReLU()
+        self.bn      = nn.BatchNorm1d(output_size)
+        self.dropout = nn.Dropout(p=0.1)
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+
+        x = self.fc(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.sigmoid(x)
+        return x
+
+def reshape_clips_to_patient_level(clip_predictions,datasets,uid_indices,labels=None):
+
+    # Make a pandas dataframe of sleep stage, prediction, and uid so we can use pandas groupby for easier restructure
+    patient_features  = []
+    patient_labels    = []
+    categorcial_block = datasets['categorical']
+    for unique_uid in uid_indices.keys():
+        
+        # Break up the user ids and get the indices we need to subslice
+        unique_uid_indices = uid_indices[unique_uid]
+        categorical_by_uid = categorcial_block[unique_uid_indices]
+        prediction_by_uid  = clip_predictions[unique_uid_indices]
+
+        # Make the user level labels for training datasets
+        if labels != None:
+            label_by_uid = labels[unique_uid_indices]
+            patient_labels.append(label_by_uid[0])
+
+        # Loop over the sleep stages
+        predictions_by_categorical = []
+        for idx in range(categorical_by_uid.shape[1]):
+            
+            # Get the rows in this subslice that match the given sleep stage
+            cat_inds = (categorical_by_uid[:,idx]==1)
+
+            # If we found data for this sleep stage, calculate posterior distribution
+            if cat_inds.sum() > 1:
+
+                # Store the fractional number of samples
+                fractional_sample_size = cat_inds.sum()/cat_inds.size(0)
+
+                # Get the list of priors
+                prior_predictions = prediction_by_uid[cat_inds]
+                
+                # Get the posterior distribution
+                posterior_predictions_no_normalization = torch.prod(prior_predictions,axis=0,dtype=torch.float64)
+                posterior_predictions                  = posterior_predictions_no_normalization/torch.sum(posterior_predictions_no_normalization)
+                
+                # Append the predictions to the new output object
+                predictions_by_categorical.extend([posterior_predictions])
+                predictions_by_categorical.append(fractional_sample_size.unsqueeze(0))
+            else:
+                placeholder = torch.tensor([0,0,0])
+                predictions_by_categorical.extend(placeholder)
+        
+        # Store the patient level features to a list
+        patient_features.append(torch.cat(predictions_by_categorical))
+    
+    # Make the patient level features into a tensor object
+    patient_features = torch.cat([t.unsqueeze(0) for t in patient_features],dim=0)
+
+    # If working with training data, make the label object a tensor object
+    if labels != None:
+        patient_labels = torch.cat([t.unsqueeze(0) for t in patient_labels],dim=0)
+
+    return patient_features,patient_labels
 
 def train_pnes(config,DL_object):
+    """
+    Function that manages the workflow for the MLP model.
+    """
 
     # Unpack the data for our model
     model_block       = DL_object[0]
-    train_transformed = DL_object[1]
+    train_transformed = DL_object[1][::20]
     test_transformed  = DL_object[2]
 
-    # Make the tensor objects
+    # Define the dictionaries that store the different network options
     train_datasets      = {}
     test_datasets       = {}
     input_dict          = {}
     hidden_dict         = {}
     dropout_dict        = {}
     subnetwork_size_out = 0
+
+    # Make the UID tensor for the final consensus score
+    uid_train_indices = train_transformed.groupby(['uid']).indices
+    uid_test_indices  = test_transformed.groupby(['uid']).indices
+
+    # Make the tensor objects for the main blocks
     for iblock in model_block:
 
         # Get the correct column subset
@@ -199,28 +280,76 @@ def train_pnes(config,DL_object):
     train_loader  = DataLoader(train_tensor_dataset, batch_size=config['batchsize'], shuffle=True)
 
     # Make the model
-    #model = CombinedNetwork(input_dict, hidden_dict, dropout_dict, combination_dict, output_size, normorder=config['normorder'], activation=config['activation'])
-    model = ConsensusNetwork(config, input_dict, hidden_dict, dropout_dict, combination_dict, output_size)
+    clip_model      = CombinedNetwork(input_dict, hidden_dict, dropout_dict, combination_dict, output_size, normorder=config['normorder'], activation=config['activation'])
+    consensus_model = ConsensusNetwork(config, input_dict, hidden_dict, dropout_dict, combination_dict, output_size)
 
     # Define the loss criterion
-    criterion = nn.BCELoss()
+    clip_criterion    = nn.BCELoss()
+    patient_criterion = nn.BCELoss()
 
     # Select the optimizer
-    optimizer = optim.Adam(model.parameters(), lr=config['lr'])
+    clip_optimizer      = optim.Adam(clip_model.parameters(), lr=config['lr'])
+    consensus_optimizer = optim.Adam(consensus_model.parameters(), lr=config['lr'])
 
     # Train the model
     num_epochs = 2
+    nbatch     = int(np.ceil(train_transformed.shape[0]/config['batchsize']))
     for epoch in range(num_epochs):
-        model.train()
-        for ibatch in train_loader:
-            optimizer.zero_grad()
-            outputs = model(*ibatch)
-            #loss    = criterion(outputs, labels)
-            #loss.backward()
-            #optimizer.step()
 
-            print(outputs)
-            exit()
+        # Handle the clip training
+        clip_predictions = []
+        clip_model.train()
+        for ibatch in tqdm(train_loader, total=nbatch, desc=f"Training Epoch {epoch:02d}", leave=False):
+            clip_optimizer.zero_grad()
+            print("A")
+            outputs   = clip_model(*ibatch)
+            print("B")
+            clip_loss = clip_criterion(outputs, ibatch[-1])
+            print("C")
+            clip_loss.backward()
+            print("D")
+            clip_optimizer.step()
+            print("E")
+            clip_predictions.append(outputs)
+            print("F")
+
+        # Make a new input tensor with all of the clips
+        clip_predictions = torch.cat(clip_predictions,dim=0)
+
+        # Get the patient level features
+        patient_train_features,patient_train_labels = reshape_clips_to_patient_level(clip_predictions,train_datasets,uid_train_indices,labels=train_targets)
+        patient_train_features = patient_train_features.float()
+
+        # Send the patient level predictions to the consensus model
+        consensus_model.train()
+        outputs      = consensus_model(patient_train_features)
+        patient_loss = patient_criterion(outputs, patient_train_labels)
+
+    # Evaluate the model on the test set
+    clip_model.eval()
+    consensus_model.eval()
+    with torch.no_grad():
+
+        # Get the train AUC
+        clip_predictions                            = clip_model(train_datasets['frequency'],train_datasets['time'],train_datasets['categorical'],train_targets)
+        patient_train_features,patient_train_labels = reshape_clips_to_patient_level(clip_predictions,train_datasets,uid_train_indices,labels=train_targets)
+        patient_train_features                      = patient_train_features.float()
+        y_pred                                      = consensus_model(patient_train_features).squeeze().numpy()
+        y_pred_clean                                = np.round(y_pred)
+        y_meas_clean                                = patient_train_labels
+        train_auc                                   = roc_auc_score(y_meas_clean,y_pred_clean)
+
+        # Get the test AUC
+        clip_predictions                            = clip_model(test_datasets['frequency'],test_datasets['time'],test_datasets['categorical'],test_targets)
+        patient_test_features,patient_test_labels   = reshape_clips_to_patient_level(clip_predictions,train_datasets,uid_test_indices,labels=train_targets)
+        patient_test_features                       = patient_test_features.float()
+        y_pred                                      = consensus_model(patient_test_features).squeeze().numpy()
+        y_pred_clean                                = np.round(y_pred)
+        y_meas_clean                                = patient_test_labels
+        test_auc                                    = roc_auc_score(y_meas_clean,y_pred_clean)
+        
+        print(f"Train AUC:",train_auc)
+        print(f"Test AUC:",test_auc)
 
 class tuning_manager:
 
@@ -232,8 +361,8 @@ class tuning_manager:
         # Save variables from the front end
         self.DL_object         = DL_object
         self.model_block       = DL_object[0]
-        self.train_transformed = DL_object[1]
-        self.test_transformed  = DL_object[2]
+        self.train_transformed = DL_object[1][::20]
+        self.test_transformed  = DL_object[2][::20]
         self.ncpu              = ncpu
         self.ntrial            = 2
         self.raydir            = '/Users/bjprager/Documents/GitHub/scalp_deep-learning/user_data/derivative/FEATURE_EXTRACTION/DEV/HYPERPARAMTER_TUNING/RAY/'
@@ -257,7 +386,7 @@ class tuning_manager:
             self.config[iblock] = {}
 
             # Hidden size selection methods. Currently limiting the max number of layers to three
-            self.config[iblock]['nlayer']  = tune.randint(0, 3)
+            self.config[iblock]['nlayer']  = tune.randint(1, 3)
             self.config[iblock]["hsize_1"] = tune.uniform(0.05, 1.5)
             self.config[iblock]["hsize_2"] = tune.uniform(0.05, 1.5)
             self.config[iblock]["hsize_3"] = tune.uniform(0.3, 1.5)
