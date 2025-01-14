bafcdeaaef7f6c1b7fd4cfcedc14fc29cecbc6f2
UPennBJPrager <bjprager@seas.upenn.edu>
2025-01-14 14:12:47 -0500

    Update mlp_tuner.py

--- scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
+++ scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
@@ -264,13 +264,11 @@ def train_pnes(config,DL_object,debug=False,patient_level=False,directload=False
     Function that manages the workflow for the MLP model.
     """
 
-    print("A")
     # Unpack the data for our model
     model_block       = DL_object[0]
     train_transformed = DL_object[1]
     test_transformed  = DL_object[2]
 
-    print("B")
     # Define the dictionaries that store the different network options
     train_datasets      = {}
     test_datasets       = {}
@@ -279,12 +277,10 @@ def train_pnes(config,DL_object,debug=False,patient_level=False,directload=False
     dropout_dict        = {}
     subnetwork_size_out = 0
 
-    print("C")
     # Make the UID tensor for the final consensus score
     uid_train_indices = train_transformed.groupby(['uid']).indices
     uid_test_indices  = test_transformed.groupby(['uid']).indices
 
-    print("D")
     # Get the output info
     outcols       = model_block['target']
     train_arr     = train_transformed[outcols].values.astype(np.float32)
@@ -293,7 +289,6 @@ def train_pnes(config,DL_object,debug=False,patient_level=False,directload=False
     test_targets  = torch.from_numpy(test_arr)
     output_size   = len(outcols)
 
-    print("E")
     # Make the tensor objects for the main blocks
     for iblock in model_block:
 
@@ -340,8 +335,6 @@ def train_pnes(config,DL_object,debug=False,patient_level=False,directload=False
             dropout_dict[iblock] = dsizes
             subnetwork_size_out += final_layer_size
 
-
-    print("F")
     # Define the combined network
     nlayer = config['combined_nlayer']
     hsizes = []
