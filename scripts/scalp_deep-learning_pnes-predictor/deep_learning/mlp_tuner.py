a3d321ae930ef69e400ef01024314e9c4fb8dff7
UPennBJPrager <bjprager@seas.upenn.edu>
2025-01-13 20:32:45 -0500

    Update mlp_tuner.py

--- scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
+++ scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
@@ -535,7 +535,7 @@ class tuning_manager:
         tuner = tune.Tuner(trainable_with_parameters,param_space=self.config,
                            tune_config=tune.TuneConfig(num_samples=self.ntrial, search_alg=hyperopt_search),
                            run_config=RunConfig(storage_path=self.raydir, name="pnes_experiment",verbose=1,
-                                                failure_config=tune.FailureConfig(fail_fast=False)))
+                                                failure_config=train.FailureConfig(fail_fast=True)))
 
         # Get the hyper parameter search results
         results = tuner.fit()
