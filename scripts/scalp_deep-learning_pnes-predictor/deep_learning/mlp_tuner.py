487c72d1a0314d55e085f7dc7f685dddb41b8a6b
UPennBJPrager <bjprager@seas.upenn.edu>
2025-01-19 12:17:22 -0500

    Update mlp_tuner.py

--- scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
+++ scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
@@ -675,7 +675,7 @@ class tuning_manager:
         """
 
         # Set the random seed
-        torch.manual_seed(42)
+        torch.manual_seed(42+8)
 
         # Save variables from the front end
         self.DL_object         = DL_object
