918c5e9cc0c84a08161be27f0362e81ec391e570
UPennBJPrager <bjprager@seas.upenn.edu>
2025-02-05 13:17:49 -0500

    Update mlp_tuner.py

--- scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
+++ scripts/scalp_deep-learning_pnes-predictor/deep_learning/mlp_tuner.py
@@ -303,6 +303,9 @@ class clip_to_consensus:
             for jdx,jvalue in enumerate(ivalue):
                 if jvalue == None:
                     consensus_posterior_raw[ikey][jdx] = self.reference_tensor
+                foo = [consensus_posterior_raw[ikey][jdx],consensus_weighting_raw[ikey][jdx]]
+                print(foo)
+                exit()
                 consensus_posterior_raw[ikey][jdx] = torch.cat([consensus_posterior_raw[ikey][jdx],consensus_weighting_raw[ikey][jdx]],dim=1)
 
         print(consensus_posterior_raw)
