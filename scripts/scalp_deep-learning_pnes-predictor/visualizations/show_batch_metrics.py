6f721d5cfef15a745652ea67a17a9f768771cacc
UPennBJPrager <bjprager@seas.upenn.edu>
2025-02-13 09:47:13 -0500

    Update show_batch_metrics.py

--- scripts/scalp_deep-learning_pnes-predictor/visualizations/show_batch_metrics.py
+++ scripts/scalp_deep-learning_pnes-predictor/visualizations/show_batch_metrics.py
@@ -1,5 +1,6 @@
 import pandas as PD
 from sys import argv
+from torch import load
 from pathlib import Path
 
 if __name__ == '__main__':
@@ -13,5 +14,15 @@ if __name__ == '__main__':
     DF             = PD.DataFrame()
     DF['folder']   = [ival.split('/train_pnes_handler_')[1].split('_')[0] for ival in files]
     DF['checknum'] = [ival.split('checkpoint_')[1].split('/')[0] for ival in files]
+
+    # Get the metrics
+    sensitivity = []
+    npv         = []
+    for ifile in files:
+        metrics = torch.load(ifile)['metrics']
+        sensitivity.append(metrics["Test_Sen"])
+        npv.append(metrics["Test_NPV"])
+    DF['Test_Sensitivity'] = sensitivity
+    DF['Test_NPV']         = npv
 
     print(DF)
